aef7d130017445d2000a1e4b4d0c7b09cb93a154
Naitives <support@naitives.com>
2024-12-22 01:48:19 +0100

    Varios arreglos para que aparezca el login

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
+// filepath: /Users/Jluquef/prueba_copilot/fancy-login-form/src/app.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'styles')));
-app.use(express.json());
+// Sirve archivos estáticos desde el directorio 'src'
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// filepath: /Users/Jluquef/prueba_copilot/fancy-login-form/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginForm from './components/LoginForm';
+
+ReactDOM.render(<LoginForm />, document.getElementById('root'));
